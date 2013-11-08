d1577d50f31a50a909667825007929be096f9907
xroom <xroom2008@gmail.com>
2013-11-08 22:57:12 +0800

    更改默认排序

--- Main/Lib/Action/ObAction.class.php
+++ Main/Lib/Action/ObAction.class.php
@@ -32,7 +32,7 @@ class ObAction extends PublicAction {
 		$Page       = new Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数
 		$show       = $Page->show();// 分页显示输出
 		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-		$list = $Model->order('mobile')->where($map)->limit($Page->firstRow.','.$Page->listRows)->select();
+		$list = $Model->order('id')->where($map)->limit($Page->firstRow.','.$Page->listRows)->select();
 		$this->assign('list',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 
